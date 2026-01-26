5ada6f3b4a474f9f18597aeca0b41b20d52d6b9c
snovatried <aaronmachuca19@gmail.com>
2026-01-26 15:32:39 -0500

    b
    Added session cleanup and cookie destruction on logout.

--- auth/logout.php
+++ auth/logout.php
@@ -1,4 +1,17 @@
 <?php
 session_start();
+// Unset all session variables
+$_SESSION = [];
+// Destroy the session cookie if set
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+// Destroy the session
 session_destroy();
+// Redirect to login
 header("Location: ../index.php");
+exit;
